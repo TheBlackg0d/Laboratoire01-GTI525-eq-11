be48d02a24f0e00ed841ef05e7fb2e1265b6672c
Franck Assan <assanfranckpatrick91@gmail.com>
2025-01-21 05:40:42 -0500

    page statistique avec pugjs et express

---
+++ public/javascripts/statistiques.js
@@ -0,0 +1,5 @@
+import DataTable from 'datatables.net';
+
+$(document).ready(function() {
+    $('#myTable').DataTable();
+  });
---
+++ routes/statistiques.js
@@ -0,0 +1,12 @@
+// var express = require('express');
+// var router = express.Router();
+
+
+// router.get('/statistique', function(req, res, next) {
+//   res.render('statistique', { 
+//       title: 'Franck',
+      
+//     });
+// });
+
+// module.exports = router;

7f160f18b3e1387e96b7015529aa920fb83f399d
TheBlackg0d <50238892+TheBlackg0d@users.noreply.github.com>
2025-01-27 18:17:36 -0500

    v1

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var papaparse = require('papaparse');
+var papaparse = require("papaparse");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-
 /* GET Statistique page */
-router.get('/statistique', async function(req, res, next) {
-
-  
+router.get("/statistique", async function (req, res, next) {
   try {
     // Fetch data
-    let response = await fetch('http://localhost:3000/data/compteurs.csv');
+    let response = await fetch("http://localhost:3000/data/compteurs.csv");
     let csvData = await response.text();
 
     // Parse data
     let data = papaparse.parse(csvData, {
       delimiter: ",",
-      header: true
+      header: true,
     });
-    
+
     console.log(data.data[0]);
-    res.render('statistique', {
-       title: 'Statistiques', 
-       listCompteurs: data.data
+    res.render("statistique", {
+      title: "Statistiques",
+      listCompteurs: data.data,
     });
   } catch (error) {
-    console.error('Error fetching or parsing data:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error fetching or parsing data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/pointInteret", async (req, res) => {
+  try {
+    let response = await fetch("http://localhost:3000/data/fontaines.csv");
+    let csvData = await response.text();
+    let data = papaparse.parse(csvData, {
+      delimiter: ",",
+      header: true,
+    });
+
+    console.log(data.data[0]);
+    const uniqueArrondissements = [
+      ...new Set(data.data.map((item) => {
+        let i = {};
+        i.Arrondissement = item.Arrondissement
+        i.
+      })),
+    ]; // [ 'A', 'B']
+    res.render("pointInteret", {
+      title: "Point d'interet",
+      listFontaines: data.data,
+      uniqueArrondissements,
+    });
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 

d708014aa79f64fbf402801eb179f83bd1f596a7
Anthony Tremblay <anthony.tremblay.3@ens.etsmtl.ca>
2025-03-10 18:58:57 -0400

    debut

--- public/javascript/pointInteret.js
+++ public/javascript/pointInteret.js
@@ -12,14 +12,43 @@ $(document).ready(function () {
       url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/French.json", // French translation (optional)
     },
   });
-
-  $("#arrondissement").on("change", () => {
-    let searchData = $(this).find(":selected").val();
-
-    if (searchData == "Tous") {
-      searchData = "";
-    }
-    console.log(searchData);
-    dataTable.search(searchData).draw();
-  });
 });
+//   // Initialize the map
+//   var map = L.map('map').setView([45.5017, -73.5673], 11);
+
+//   // Add a tile layer to the map
+//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(map);
+
+//   // Load GeoJSON data
+//   $.getJSON('/path/to/territoires.geojson', function(data) {
+//     var geojsonLayer = L.geoJson(data, {
+//       style: function(feature) {
+//         return { color: "#3388ff" };
+//       }
+//     }).addTo(map);
+
+//     // Handle arrondissement selection
+//     $('#arrondissement').change(function() {
+//       var selectedArrondissement = $(this).val();
+//       geojsonLayer.eachLayer(function(layer) {
+//         if (layer.feature.properties.name === selectedArrondissement) {
+//           layer.setStyle({ color: "#ff7800" });
+//         } else {
+//           layer.setStyle({ color: "#3388ff" });
+//         }
+//       });
+//     });
+//   });
+
+//   $("#arrondissement").on("change", () => {
+//     let searchData = $(this).find(":selected").val();
+
+//     if (searchData == "Tous") {
+//       searchData = "";
+//     }
+//     console.log(searchData);
+//     dataTable.search(searchData).draw();
+//   });
+// });

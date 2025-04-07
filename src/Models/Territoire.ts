13cd0447769e211e000db30203e107c32f7760a3
Anthony Tremblay <zab2x3z@gmail.com>
2025-04-07 16:30:34 -0400

    use db instead of geojson WIP

--- src/Models/Territoire.ts
+++ src/Models/Territoire.ts
@@ -2,8 +2,14 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface ITerritoire {
   nom: string;
-  codeID: string;
+  codeID: number;
   abrev: string;
+  type: string;
+  comment?: string | null;
+  dateModif: string;
+  codeMAMH: string;
+  num: number;
+  geometry: object;
 }
 
 export interface ITerritoireModel extends ITerritoire, Document {}
@@ -11,8 +17,14 @@ export interface ITerritoireModel extends ITerritoire, Document {}
 const TerritoireSchema: Schema = new Schema(
   {
     nom: { type: String, required: true },
-    codeID: { type: String, required: true },
+    codeID: { type: Number, required: true },
     abrev: { type: String, required: true },
+    type: { type: String, required: true },
+    comment: { type: String, default: null },
+    dateModif: { type: String, required: true },
+    codeMAMH: { type: String, required: true },
+    num: { type: Number, required: true },
+    geometry: { type: Object, required: true },
   },
   {
     versionKey: false,
--- src/controllers/TerritoireController.ts
+++ src/controllers/TerritoireController.ts
@@ -38,19 +38,45 @@ export default class TerritoireController {
     }
   }
 
-  async getGeoData(): Promise<String> {
-    try {
-      // Fetch data
-      let response = await fetch(
-        "http://localhost:3000/data/territoires.geojson"
-      );
-      let geojson = await response.json();
-      return geojson;
-    } catch (error) {
-      console.error("Error fetching or parsing data:", error);
-    }
+  async getGeoData(): Promise<string> {
+    // try {
+    //   // Fetch data
+    //   let response = await fetch(
+    //     "http://localhost:3000/data/territoires.geojson"
+    //   );
+    //   let geojson = await response.json();
+    //   return geojson;
+    // } catch (error) {
+    //   console.error("Error fetching or parsing data:", error);
+    // }
 
-    return "";
+    const territoires = await Territoire.find();
+
+    const geojson = {
+      type: "FeatureCollection",
+      features: territoires.map((territoire) => ({
+        type: "Feature",
+        properties: {
+          CODEID: territoire.codeID,
+          NOM: territoire.nom,
+          CODEMAMH: territoire.codeMAMH,
+          NUM: territoire.num,
+          ABREV: territoire.abrev,
+          TYPE: territoire.type,
+          COMMENT: territoire.comment,
+          DATEMODIF: territoire.dateModif
+        },
+        geometry: territoire.geometry,
+      })),
+      crs: {
+        type: "name",
+        properties: {
+          name: "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+      }
+    };
+    
+    return JSON.stringify(geojson);
   }
 
   async delete(territoireId: number) {
